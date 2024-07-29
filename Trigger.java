46b26ae30e7d388eb36019a5ffc5fc65fda5cab5
Dharshini212 <148734202+Dharshini212@users.noreply.github.com>
2024-07-29 20:10:26 +0530

    Add files via upload

---
+++ Trigger.java
@@ -0,0 +1,19 @@
+
+public class Trigger {
+    private final String condition;
+    private final String action;
+
+    public Trigger(String condition, String action) {
+        this.condition = condition;
+        this.action = action;
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public String getAction() {
+        return action;
+    }
+}
+
